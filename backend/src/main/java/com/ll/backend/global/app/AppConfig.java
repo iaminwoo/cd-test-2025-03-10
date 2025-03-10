5dcff124b0b447d83bea5a64d5e664899f57f253
inwoo <dlsdn1108@gmail.com>
2025-03-10 17:15:35 +0900

    AppConfig 수정

--- backend/src/main/java/com/ll/backend/global/app/AppConfig.java
+++ backend/src/main/java/com/ll/backend/global/app/AppConfig.java
@@ -3,23 +3,53 @@ package com.ll.backend.global.app;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private static Environment environment;
     @Getter
     private static ObjectMapper objectMapper;
+    @Getter
+    private static String siteFrontUrl;
+    @Getter
+    private static String siteBackUrl;
+    @Getter
+    private static String siteCookieDomain;
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        AppConfig.environment = environment;
+    }
 
     @Autowired
     public void setObjectMapper(ObjectMapper objectMapper) {
         AppConfig.objectMapper = objectMapper;
     }
 
-    public static boolean isNotProd() {
-        return true;
+    @Value("${custom.site.frontUrl}")
+    public void setSiteFrontUrl(String siteFrontUrl) {
+        AppConfig.siteFrontUrl = siteFrontUrl;
     }
 
-    public static String getSiteFrontUrl() {
-        return "http://localhost:3000";
+    @Value("${custom.site.backUrl}")
+    public void setSiteBackUrl(String siteBackUrl) {
+        AppConfig.siteBackUrl = siteBackUrl;
+    }
+
+    @Value("${custom.site.cookieDomain}")
+    public void setSiteCookieDomain(String siteCookieDomain) {
+        AppConfig.siteCookieDomain = siteCookieDomain;
     }
 }
